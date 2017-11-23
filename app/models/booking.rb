6e34bd23eb9aade5b6467f443a7734a5b5106cc5
jules maregiano <jules.maregiano@gmail.com>
2017-11-23 11:24:27 +0100

    création des routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
+  belongs_to :technician, class_name: "User", foreign_key: "user_id"
+  has_many :availabilities
+  validates :user_id, presence: :true
   belongs_to :user
-  belongs_to :availabilities
+
+  def booker
+    self.user
+  end
+
+  def technician
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Availabilities.destroy_all
+Bookings.destroy_all
+
+particulier = User.create!(email: "particulier@led.fr", password: "123soleil", first_name: "Parti", last_name: "Culier", phone:"06 11 22 33 44", role:0)
+pro = User.create!(email: "pro@led.fr", password: "123soleil", first_name: "Pro", last_name: "Fessionnel", phone:"06 11 22 33 44", role:1)
+technician = User.create!(email: "tech@led.fr", password: "123soleil", first_name: "Tech", last_name: "Nician", phone:"06 11 22 33 44", role:2)
+manager = User.create!(email: "manager@led.fr", password: "123soleil", first_name: "Ma", last_name: "Nager", phone:"06 11 22 33 44", role:3)
+admin = User.create!(email: "admin@led.fr", password: "123soleil", first_name: "Ad", last_name: "Min", phone:"06 11 22 33 44", role:4)
+puts "#{User.all.size} Users créés."
+
+
+next_25 = (1..25).to_a
+next_25.each do |numero|
+  User.all.where(role: 1).each do |user|
+    date = numero.business_days.from_now
+    hours = [8, 14]
+    Availability.find_or_create_by(user_id: user.id, date: Time.new(date.year, date.month, date.day, hours[0], 0, 0, 0) )
+    Availability.find_or_create_by(user_id: user.id, date: Time.new(date.year, date.month, date.day, hours[1], 0, 0, 0) )
+  end
+puts "#{Availabilities.all.size} Availabilities créées."
