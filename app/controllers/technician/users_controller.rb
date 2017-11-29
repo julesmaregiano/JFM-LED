5cd6d21b8150aba5e4e5742da80159d3eb61b720
jules maregiano <jules.maregiano@gmail.com>
2017-11-29 15:08:01 +0100

    modif controllers pour finaliser map

--- app/controllers/technician/users_controller.rb
+++ app/controllers/technician/users_controller.rb
@@ -4,8 +4,8 @@ class Technician::UsersController < ApplicationController
     @user = current_user
     @bookings_for_map = Booking.where.not(latitude: nil, longitude: nil)
     @markers = Gmaps4rails.build_markers(@bookings_for_map) do |booking, marker|
-      marker.lat flat.latitude
-      marker.lng flat.longitude
+      marker.lat booking.latitude
+      marker.lng booking.longitude
     end
   end
 
