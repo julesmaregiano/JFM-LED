c2bdd76ed40734a0bccd87d76b4d72ecad19eeb6
jules maregiano <jules.maregiano@gmail.com>
2017-11-30 19:47:38 +0100

    Ajout des pages detection, diagnostic et societe + css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/eligibility', to: 'pages#eligibility'
+  get '/detection', to: 'pages#detection'
+  get '/diagnostic', to: 'pages#diagnostic'
+  get '/societe', to: 'pages#societe'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :pro do
