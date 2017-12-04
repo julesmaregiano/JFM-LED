394868d4f69992932f2eccb9cb2ca3e9658851c9
jules maregiano <jules.maregiano@gmail.com>
2017-12-04 12:44:03 +0100

    ajout de la gem simple_calendar. render du calendar dans technician#availabilities#index

--- app/controllers/technician/availabilities_controller.rb
+++ app/controllers/technician/availabilities_controller.rb
@@ -2,6 +2,6 @@ class Technician::AvailabilitiesController < ApplicationController
 
   def index
     @user = current_user
-    @availabilities = Availability.all
+    @availabilities = Availability.all.where(user: @user)
   end
 end
