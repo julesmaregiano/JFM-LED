2daec293e738e52de690e56527d0c21dcc486eb6
jules maregiano <jules.maregiano@gmail.com>
2017-12-07 11:12:36 +0100

    manager can now modify all techs calendars

--- app/controllers/manager/availabilities_controller.rb
+++ app/controllers/manager/availabilities_controller.rb
@@ -2,6 +2,23 @@ class Manager::AvailabilitiesController < ApplicationController
 
   def index
     @user = current_user
+    @techs = User.all.where(role: 2)
     @availabilities = Availability.all
+  end
+
+  def update
+    @user = current_user
+    @availability = Availability.find(params[:id])
+    if @availability.update(params_availability)
+      redirect_to manager_availabilities_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def params_availability
+    params.require(:availability).permit(:status, :user_id)
   end
 end
