fe736b2e8e82a12664d91976e9bdf448fed1b3b6
jules maregiano <jules.maregiano@gmail.com>
2017-12-13 11:30:05 +0100

    modifs sur les controllers & views pour availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -8,10 +8,10 @@ class Availability < ApplicationRecord
   scope :to_come, -> { where("date >= ?", Date.today) }
   scope :not_today, -> { where.not("date = ?", Date.today) }
   scope :of, -> (user) {where("user_id = ? ", user.id)}
-  scope :booked, -> { where.not(booking_id: nil) }
+  scope :booked, -> { where(status: 0) }
   scope :oldest_to_new, -> { order(created_at: :asc)}
 
-  enum role: [:booked, :free, :pending, :leave]
+  enum status: [:booked, :free, :pending, :leave]
 
   def init
     self.update(status: 1)
