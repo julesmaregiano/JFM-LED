a9ed87d9daae00e6e62ab4a986087f6a0a7da7c0
jules maregiano <jules.maregiano@gmail.com>
2018-01-03 11:19:18 +0100

    ajout du product au Pro Booking#create

--- app/controllers/pro/bookings_controller.rb
+++ app/controllers/pro/bookings_controller.rb
@@ -27,19 +27,21 @@ class Pro::BookingsController < ApplicationController
     @user = current_user
     @tech = User.where(role: 3).first
     @availabilities = Availability.to_come.not_today.free_first
+    @products = Product.all
     @booking = Booking.new
   end
 
   def create
     @user = current_user
     @tech = User.where(role: 3).first
+    @products = Product.all
     @foremen = Foreman.where(branch_id: @user.branch_id).to_a
     @availabilities = Availability.all
     @booking = Booking.new(booking_params)
     @booking.user_id = @user.id
-    @booking.report = Report.new
     if @booking.save
       @booking.availabilities.update(status: "pending")
+      Report.create(booking_id: @booking.id)
       redirect_to pro_user_path(@user)
     else
       render :new
@@ -47,7 +49,7 @@ class Pro::BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:comment, :foreman_id, :address1, :address2, :zipcode, :city, :country, :surface, availability_ids: [])
+    params.require(:booking).permit(:comment, :foreman_id, :address1, :address2, :zipcode, :city, :country, :product_id, :surface, availability_ids: [])
   end
 
 end
