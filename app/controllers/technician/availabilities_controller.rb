28612a783ef0e7c530a20ac9240097fffdd5b47c
jules maregiano <jules.maregiano@gmail.com>
2018-01-03 18:07:08 +0100

    Suppression de la possibilité d'updater ses availabilities pour le tech

--- app/controllers/technician/availabilities_controller.rb
+++ app/controllers/technician/availabilities_controller.rb
@@ -5,15 +5,17 @@ class Technician::AvailabilitiesController < ApplicationController
     @availabilities = Availability.to_come.of(@user)
   end
 
-  def update
-    @user = current_user
-    @availability = Availability.find(params[:id])
-    if @availability.update(params_availability)
-      redirect_to technician_availabilities_path
-    else
-      render :edit
-    end
-  end
+  # Pour le moment, les techs doivent passer par le manager pour se mettre indisponibles. Décommenter et mettre le form dans le calendar pour rétablir.
+  #
+  # def update
+  #   @user = current_user
+  #   @availability = Availability.find(params[:id])
+  #   if @availability.update(params_availability)
+  #     redirect_to technician_availabilities_path
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   private
 
