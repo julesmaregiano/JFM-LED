cacb7cd9d64c013e2f82183dd6537da92150bfff
jules maregiano <jules.maregiano@gmail.com>
2018-01-08 12:06:33 +0100

    Ajout des branches et company pour faire marcher le sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :phone, :role, :password, :password_confirmation, :email, :remember_me]
+    added_attrs = [:first_name, :last_name, :phone, :role, :password, :password_confirmation, :email, :remember_me, :branch_id, :company_id]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
