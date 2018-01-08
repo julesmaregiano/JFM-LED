adc2bcb553b0e9912da8cd7adbcc99ff4f281579
jules maregiano <jules.maregiano@gmail.com>
2018-01-08 10:30:11 +0100

    passer du controller booking a foremen

--- app/controllers/pro/bookings_controller.rb
+++ app/controllers/pro/bookings_controller.rb
@@ -47,13 +47,6 @@ class Pro::BookingsController < ApplicationController
     else
       render :new
     end
-
-    @foreman = Foreman.new(params_foreman)
-    if @foreman.save
-      redirect_to new_pro_booking_path and return
-    else
-      render :new
-    end
   end
 
   private
--- app/controllers/pro/foremen_controller.rb
+++ app/controllers/pro/foremen_controller.rb
@@ -3,7 +3,7 @@ class Pro::ForemenController < ApplicationController
   def create
     @foreman = Foreman.new(params_foreman)
     if @foreman.save
-      redirect_to pro_booking_path
+      redirect_to new_pro_booking_path and return
     else
       render :new
     end
