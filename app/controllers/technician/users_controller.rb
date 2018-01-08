e38ef7c20987fe05ef21d4e535117cbb6336f220
jules maregiano <jules.maregiano@gmail.com>
2018-01-08 16:05:16 +0100

    deplacement de user#show vers #dashboard pour le tech

--- app/controllers/technician/users_controller.rb
+++ app/controllers/technician/users_controller.rb
@@ -2,13 +2,6 @@ class Technician::UsersController < ApplicationController
 
   def show
     @user = current_user
-    @availabilities = Availability.of_the_week_for(@user)
-    @bookings = Booking.for_next_week_for(@user)
-    @bookings_for_map = @bookings.delete_if { |booking| booking.nil? || booking.latitude.nil? }
-    @markers = Gmaps4rails.build_markers(@bookings_for_map) do |booking, marker|
-      marker.lat booking.latitude
-      marker.lng booking.longitude
-    end
   end
 
   def index
