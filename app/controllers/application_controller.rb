bf5980158efacf0e0d8c1b9ac0767611d3ca0cda
jules maregiano <jules.maregiano@gmail.com>
2018-01-09 11:28:26 +0100

    modif sur date_range de calendar, correction redirect app controller, correction sur seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-     "/#{resource.role}/users/#{resource.id}"
+     "/#{resource.role}/dashboard"
   end
 
   protected
