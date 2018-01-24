7a53bb66d96a5995f84d479554952e974e3637d0
jules maregiano <jules.maregiano@gmail.com>
2018-01-24 14:30:58 +0100

    compact anti nil

--- app/controllers/technician/dashboards_controller.rb
+++ app/controllers/technician/dashboards_controller.rb
@@ -4,7 +4,7 @@ class Technician::DashboardsController < ApplicationController
     @user = current_user
     @bookings_hash = {}
     dates = Date.today.business_dates_until(Date.today + 7)
-    dates.each { |d| @bookings_hash[d] = @user.availabilities.where(date: d).map { |a| a.booking }.uniq }
+    dates.each { |d| @bookings_hash[d] = @user.availabilities.where(date: d).map { |a| a.booking }.uniq.compact }
 
     @bookings_for_map = Booking.for_next_week_for(@user).delete_if { |booking| booking.nil? || booking.latitude.nil? }
     @markers = Gmaps4rails.build_markers(@bookings_for_map) do |booking, marker|
