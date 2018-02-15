e63ea1e3ff4bfccadcd37c0082fb9aeeb79bbc15
jules maregiano <jules.maregiano@gmail.com>
2018-02-15 16:45:18 +0100

    clé dans secret yml

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: 'W7HIKRIOMJ', api_key: 'fc8336859348a587d2c9fd315af9528f' }
+AlgoliaSearch.configuration = { application_id: 'W7HIKRIOMJ', api_key: ALGOLIA_ADMIN_KEY }
