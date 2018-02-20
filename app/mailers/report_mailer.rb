d7581f78daba31536b92452e3476710bbc996820
jules maregiano <jules.maregiano@gmail.com>
2018-02-20 11:29:11 +0100

    mailer fonctionne avec le rapport en piece jointe

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -7,7 +7,26 @@ class ReportMailer < ApplicationMailer
   #
   def report_complete(report)
     @report = report
-    @user = @report.booking.user
-    mail to: @user.email, subject: 'Votre rapport de #{@report.booking.product.label} de votre chantier à #{@report.booking.address.city}'
+    @client = @report.user
+    @tech = @report.booking.availabilities.first.user
+    @sections = @report.booking.product.sections.uniq.sort_by(&:created_at)
+
+
+    attachments["report#{report.id}.pdf"] = WickedPdf.new.pdf_from_string(
+      render_to_string( pdf:                            "#{@report.booking.reference} Rapport de #{@report.booking.product.label}",
+                        template:                       "shared/_report.html.erb",
+                        margin:  { top:                 5,                     # default 10 (mm)
+                                   bottom:              5,
+                                   left:                0,
+                                   right:               0 },
+                        viewport_size:                  '1280x1024',
+                        save_only:                      false,
+                        background:                     true,                     # backround needs to be true to enable background colors to render
+                        no_background:                  false,
+                        encoding:                       'UTF-8'
+      )
+    )
+
+    mail to: @report.booking.user.email, subject: 'Votre rapport de #{@report.booking.product.label} de votre chantier à #{@report.booking.address.city}'
   end
 end
