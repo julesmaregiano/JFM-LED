e259dd00db54a9e49a5d08d3a01fd6d5306aed12
jules maregiano <jules.maregiano@gmail.com>
2018-02-23 10:42:58 +0100

    ajout signature a l'user et ajout d'un edit pour creer sa signature dans user#show du tech.

--- app/controllers/technician/users_controller.rb
+++ app/controllers/technician/users_controller.rb
@@ -1,11 +1,26 @@
 class Technician::UsersController < ApplicationController
 
-  def show
-    @user = current_user
-  end
-
   def index
     @user = current_user
     @users = User.where(role: [0, 1])
   end
+
+  def show
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    if @user.save
+      redirect_to [@user.role, @user]
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:signature)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221162308) do
+ActiveRecord::Schema.define(version: 20180223090900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -277,6 +277,7 @@ ActiveRecord::Schema.define(version: 20180221162308) do
     t.string "phone"
     t.bigint "company_id"
     t.bigint "branch_id"
+    t.binary "signature"
     t.index ["branch_id"], name: "index_users_on_branch_id"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
