bbe03de7e6f76537533d0743571faae53150c138
Pathe <pathe.sene@gmail.com>
2018-03-13 03:37:45 +0000

    Annotate model manager.

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,4 +1,8 @@
 class Manager < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :service_provider
 
   validates :email, :first_name, :last_name,
@@ -10,19 +14,30 @@ class Manager < ApplicationRecord
 end
 
 # == Schema Information
-# Schema version: 20180312213904
+# Schema version: 20180313033516
 #
 # Table name: managers
 #
-#  created_at          :datetime         not null
-#  email               :string
-#  first_name          :string
-#  id                  :integer          not null, primary key
-#  last_name           :string
-#  service_provider_id :integer
-#  updated_at          :datetime         not null
+#  created_at             :datetime         not null
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :inet
+#  email                  :string
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  id                     :integer          not null, primary key
+#  last_name              :string
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :inet
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  service_provider_id    :integer
+#  sign_in_count          :integer          default("0"), not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_managers_on_service_provider_id  (service_provider_id)
+#  index_managers_on_email                 (email) UNIQUE
+#  index_managers_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_managers_on_service_provider_id   (service_provider_id)
 #
