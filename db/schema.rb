ba713907b3f5647179dc103bcef69de6fd9f7a85
Pathe <pathe.sene@gmail.com>
2018-03-13 03:37:24 +0000

    Add devise columns to manager model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313031525) do
+ActiveRecord::Schema.define(version: 20180313033516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -188,6 +188,17 @@ ActiveRecord::Schema.define(version: 20180313031525) do
     t.bigint "service_provider_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["email"], name: "index_managers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
     t.index ["service_provider_id"], name: "index_managers_on_service_provider_id"
   end
 
