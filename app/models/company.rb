c0df0481674185882e66f68dccb9422129b44522
Pathe <pathe.sene@gmail.com>
2018-03-13 01:29:09 +0000

    Setup addressable for companies.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,11 @@
 class Company < ApplicationRecord
   validates :name, presence: :true
+
   has_many :users
   has_many :branches
   has_many :company_products
   has_many :products, through: :company_products
+  has_one  :address, as: :addressable
 
   has_attachment :photo
 
