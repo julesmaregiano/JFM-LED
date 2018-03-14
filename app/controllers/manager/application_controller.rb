cb89c0d791f212ca1f745f63629a58ab5f71608f
Pathe <pathe.sene@gmail.com>
2018-03-14 10:50:02 +0000

    Manager::ApplicationController inherits from ApplicationController

--- app/controllers/manager/application_controller.rb
+++ app/controllers/manager/application_controller.rb
@@ -1,4 +1,4 @@
-class Manager::ApplicationController < ActionController::Base
+class Manager::ApplicationController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_manager!
   protect_from_forgery with: :exception
