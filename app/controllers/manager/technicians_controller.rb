1565b7b5a98d85b3e4cb1d2732734fa1953b70b0
Pathe <pathe.sene@gmail.com>
2018-03-14 13:23:08 +0000

    Add index & edit actions for technicians.

--- app/controllers/manager/technicians_controller.rb
+++ app/controllers/manager/technicians_controller.rb
@@ -1,5 +1,9 @@
 class Manager::TechniciansController < Manager::ApplicationController
   def index
+    @technicians = Technician.where(service_provider_id: current_manager.service_provider_id)
+  end
 
+  def edit
+    @technician = current_manager.service_provider.technicians.find(params[:id])
   end
 end
