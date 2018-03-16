c72f9450b79f5780309bb92d7c23a8dfb0f73711
Pathe <pathe.sene@gmail.com>
2018-03-16 10:34:34 +0000

    Import client selector component in react rails.

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld.jsx';
+import ClientSelector from '../bundles/HelloWorld/components/ClientSelector.jsx';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
+  ClientSelector
 });
