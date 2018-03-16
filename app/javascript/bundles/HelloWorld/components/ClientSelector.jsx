fcd757363ccb3af3f6c86384a0a9a5886d560df6
Pathe <pathe.sene@gmail.com>
2018-03-16 10:33:58 +0000

    Update bundle

---
+++ app/javascript/bundles/HelloWorld/components/ClientSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ClientSelector extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>TEST</div>
+      </div>
+    )
+  }
+}
--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types';
 import React     from 'react';
 
-import HalfDayAvailability from './HalfDayAvailability.jsx';
-
 export default class HelloWorld extends React.Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
