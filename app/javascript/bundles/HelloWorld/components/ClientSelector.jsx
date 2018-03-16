0d667a9326fff7d7cf2d551aeea52055b1575ac9
Pathe <pathe.sene@gmail.com>
2018-03-16 12:01:23 +0000

    Add open modal.

--- app/javascript/bundles/HelloWorld/components/ClientSelector.jsx
+++ app/javascript/bundles/HelloWorld/components/ClientSelector.jsx
@@ -2,24 +2,53 @@ import React from 'react';
 import Popup from "reactjs-popup";
 
 export default class ClientSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
+  openModal = () => {
+    this.setState({ open: true });
+  };
+
+  closeModal = () => {
+    this.setState({ open: false });
+  };
+
+
   render() {
+     return (
+       <div>
+         <i className='fa fa-user fa-2x' onClick={this.openModal} />
+         <CreateClientPopup open={this.state.open}  />
+       </div>
+     );
+   }
+}
+
+class CreateClientPopup extends React.Component {
+  render() {
+    var { open } = this.props;
     return (
-      <Popup
-        trigger={<i className="fa fa-user"> Open Modal </i>}
-        modal
-        closeOnDocumentClick
-      >
+      <Popup open={open} modal closeOnDocumentClick>
         <div>
+          <br />
           <div class='input-group'>
             <label className='input-group-addon'>Nom de l'entreprise</label>
             <input type='text'className="form-control" />
           </div>
+          <br />
           <div className='input-group'>
             <label className='input-group-addon'>Siret</label>
             <input type='text' className="form-control" />
           </div>
+          <br />
+          <div className='input-group'>
+            <button>Créer ce client!</button>
+          </div>
         </div>
       </Popup>
     )
+
   }
 }
