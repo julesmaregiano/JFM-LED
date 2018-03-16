3b91d29e0024a04c338c49b47fac7e40ad408772
Pathe <pathe.sene@gmail.com>
2018-03-16 10:01:00 +0000

    Annotate.

--- app/models/foreman.rb
+++ app/models/foreman.rb
@@ -4,19 +4,21 @@ class Foreman < ApplicationRecord
 end
 
 # == Schema Information
-# Schema version: 20180312213904
+# Schema version: 20180316095812
 #
 # Table name: foremen
 #
-#  branch_id  :integer
-#  created_at :datetime         not null
-#  first_name :string
-#  id         :integer          not null, primary key
-#  last_name  :string
-#  phone      :string
-#  updated_at :datetime         not null
+#  branch_id           :integer
+#  created_at          :datetime         not null
+#  first_name          :string
+#  id                  :integer          not null, primary key
+#  last_name           :string
+#  phone               :string
+#  service_provider_id :integer
+#  updated_at          :datetime         not null
 #
 # Indexes
 #
-#  index_foremen_on_branch_id  (branch_id)
+#  index_foremen_on_branch_id            (branch_id)
+#  index_foremen_on_service_provider_id  (service_provider_id)
 #
