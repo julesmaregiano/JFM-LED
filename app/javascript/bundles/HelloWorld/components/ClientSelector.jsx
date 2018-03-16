dc52b0b69fd9525bbe2c601e9b7ab422bf408e16
Pathe <pathe.sene@gmail.com>
2018-03-16 11:21:11 +0000

    Build create_client popup.

--- app/javascript/bundles/HelloWorld/components/ClientSelector.jsx
+++ app/javascript/bundles/HelloWorld/components/ClientSelector.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Popup from "reactjs-popup";
 
 export default class ClientSelector extends React.Component {
   render() {
     return (
-      <div>
-        <div>TEST</div>
-      </div>
+      <Popup
+        trigger={<i className="fa fa-user"> Open Modal </i>}
+        modal
+        closeOnDocumentClick
+      >
+        <div>
+          <div class='input-group'>
+            <label className='input-group-addon'>Nom de l'entreprise</label>
+            <input type='text'className="form-control" />
+          </div>
+          <div className='input-group'>
+            <label className='input-group-addon'>Siret</label>
+            <input type='text' className="form-control" />
+          </div>
+        </div>
+      </Popup>
     )
   }
 }
