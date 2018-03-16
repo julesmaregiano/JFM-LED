6e6e8d5d092f6c1804926fe13e30fc6e13d37310
Pathe <pathe.sene@gmail.com>
2018-03-16 18:44:55 +0000

    Add a repository for availbilities.

---
+++ app/services/repositories/availabilities.rb
@@ -0,0 +1,15 @@
+class Repositories::Availabilities
+  def self.for_technicians(tech_ids, starts_at, ends_at)
+    Availability
+      .where(technician_id: tech_ids)
+      .of_the_week
+      .order(date: :asc)
+      .group_by {|a| a.technician }
+      .map do |key, values|
+        [
+          [key, values.select {|v| v.half.eql?('matin')}].flatten,
+          values.select {|v| v.half.eql?('aprem')},
+        ]
+      end
+  end
+end
