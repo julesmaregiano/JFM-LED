1c7c8f727bf1dd7f1a4efc22e72418a3fd6ab2fd
Pathe <pathe.sene@gmail.com>
2018-03-19 23:12:26 +0000

    Refactor availabilities repository & add a method to fetch availabilities for a user.

--- app/services/repositories/availabilities.rb
+++ app/services/repositories/availabilities.rb
@@ -1,10 +1,6 @@
 class Repositories::Availabilities
   def self.for_technicians(tech_ids, starts_at, ends_at)
-    Availability
-      .includes(:technician)
-      .where(technician_id: tech_ids)
-      .where("date  >= ? AND date <= ?", starts_at, ends_at)
-      .order(date: :asc)
+    self.availabilities(tech_ids, starts_at, ends_at)
       .group_by {|a| a.technician }
       .map do |key, values|
         [
@@ -12,5 +8,24 @@ class Repositories::Availabilities
           values.select {|v| v.half.eql?('aprem')},
         ]
       end
+  end
+
+
+  def self.for_technician(tech_id, starts_at, ends_at)
+    availables = self.availabilities([tech_id], starts_at, ends_at)
+    [
+       availables.select {|v| v.half.eql?('matin')},
+       availables.select {|v| v.half.eql?('aprem')},
+    ]
+  end
+
+  private
+
+  def self.availabilities(tech_ids, starts_at, ends_at)
+    Availability
+      .includes(:technician)
+      .where(technician_id: tech_ids)
+      .where("date  >= ? AND date <= ?", starts_at, ends_at)
+      .order(date: :asc)
   end
 end
