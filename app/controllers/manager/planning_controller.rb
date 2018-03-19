44187a1f3b6059562d7652fd19fb2ca184ea0efc
Pathe <pathe.sene@gmail.com>
2018-03-19 01:30:29 +0000

    Load availabilities in planning controller.

--- app/controllers/manager/planning_controller.rb
+++ app/controllers/manager/planning_controller.rb
@@ -1,5 +1,13 @@
 class Manager::PlanningController < ApplicationController
-
   def index
+    technician_ids  = service_provider.technician_ids
+    @availabilities = Repositories::Availabilities.for_technicians(technician_ids, Date.today, Date.today + 5.days)
+    @headers        = @availabilities.first.last.map(&:date)
+  end
+
+  private
+
+  def service_provider
+    current_manager.try(:service_provider)
   end
 end
