c816b4b8b01ddcf55b119bad40df5caa2e9de2e1
Pathe <pathe.sene@gmail.com>
2018-03-19 19:58:00 +0000

    Better fetching & calculation of dates.

--- app/controllers/manager/planning_controller.rb
+++ app/controllers/manager/planning_controller.rb
@@ -2,11 +2,12 @@ class Manager::PlanningController < Manager::ApplicationController
   def index
     technician_ids  = service_provider.technician_ids
     given           = params[:date].present? ? Date.parse(params[:date]) : Date.today
-    @beginning      = given.beginning_of_week
-    ending          = @beginning + 4.days
+    beginning       = given.beginning_of_week
+    ending          = beginning + 4.days
+    @next_date      = beginning + 7.days
 
     @availabilities = Repositories::Availabilities.for_technicians(technician_ids, beginning, ending)
-    @headers        = @availabilities.first.last.map(&:date)
+    @headers        = @availabilities.first.last.map(&:date) unless @availabilities.length.zero?
   end
 
   private
