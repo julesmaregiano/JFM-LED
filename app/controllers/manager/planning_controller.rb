077cc5d10f3b3367bc4fda48e32e5bd14cc3b09a
Pathe <pathe.sene@gmail.com>
2018-03-19 02:06:58 +0000

    Planning inherits from manager application controller.

--- app/controllers/manager/planning_controller.rb
+++ app/controllers/manager/planning_controller.rb
@@ -1,7 +1,9 @@
-class Manager::PlanningController < ApplicationController
+class Manager::PlanningController < Manager::ApplicationController
   def index
     technician_ids  = service_provider.technician_ids
-    @availabilities = Repositories::Availabilities.for_technicians(technician_ids, Date.today, Date.today + 5.days)
+    beginning       = Date.today.beginning_of_week
+    ending          = beginning + 5.days
+    @availabilities = Repositories::Availabilities.for_technicians(technician_ids, beginning, ending)
     @headers        = @availabilities.first.last.map(&:date)
   end
 
